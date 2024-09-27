e9f0ed64cccd96e9e9a8e3f19c86172df3278d7d
mokgaetjiM <mokgaetji.mashabela@ul.ac.za>
2024-09-27 10:01:26 +0200

    Add project files.

---
+++ PROJECT1/BeerMilk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT1
+{
+    internal class BeerMilk
+    {
+        public void Song()
+        {
+            // initialize to 99
+            int BottleBeerMilk = 99;
+
+            do
+            {
+                if (BottleBeerMilk > 1)
+                {
+                    Console.WriteLine(BottleBeerMilk + " bottles of beer on the wall, " + BottleBeerMilk + " bottles of beer. Take one down and pass it around,");
+                }
+                else if (BottleBeerMilk == 1)
+                {
+                    Console.WriteLine("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around.");
+                }
+
+                // Decrease the number of bottles 
+                BottleBeerMilk--;
+                // terminating statement untill all conditions are met
+            } while (BottleBeerMilk > 0);
+        }
+    }
+}
+
---
+++ PROJECT1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BeerMilk song = new BeerMilk();     //object instance of the BeerMilk class
+
+            song.Song();                        //calling method using object instance
+
+        }
+    }
+}
